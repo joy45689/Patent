dafbb9ee9a94cd7b58d102b3bc6bcc6b2d92fc65
joy45689 <joy45689acc@gmail.com>
2019-02-22 16:10:25 +0800

    Add three instances.

--- src/main/java/arslab/amber/Patent.java
+++ src/main/java/arslab/amber/Patent.java
@@ -1,5 +1,7 @@
 package arslab.amber;
 
+import java.util.List;
+
 /**
  * Hello world!
  *
@@ -10,4 +12,101 @@ public class Patent
     {
         System.out.println( "Hello World!" );
     }
+    Patent (){
+
+    }
+
+    /*instance 1*/
+    double[] enc1(double[] k, double V, double[] r){
+        int n=r.length-1;
+        double [] ciphers =new double[k.length];
+        for(int i=0;i<n;i++){
+            ciphers[i]=(k[i]*V)+r[i];
+        }
+
+        ciphers[n]=k[n]*sum(r,n);
+        return ciphers;
+    }
+
+    double dec1(double []k,double[]ciphers, double[] r){
+        double V=0;
+        int n=r.length-1;
+        double L = sum(k,n);
+        for(int i=0;i<n;i++){ //i=1~n-1
+            V+=ciphers[i]/L;
+        }
+        V=V-(ciphers[n]/(L*k[n]));
+        return V;
+    }
+    /*instance 2*/
+    double[] enc2(double[] k, double[] s, double V, double[] r, double[] p){
+        int n=r.length-1;
+        double [] ciphers =new double[k.length];
+        for(int i=0;i<n;i++){
+            ciphers[i]=k[i]*(s[i]*V+p[i])+r[i];
+        }
+        ciphers[n]=s[n]*k[n]*sum(p,r,k,n);
+        return ciphers;
+    }
+    double dec2(double[] k, double[] s, double[] ciphers, double[] r, double[] p){
+        double V=0;
+        int n=r.length-1;
+        double S=sum(s,n);
+        for(int i=0;i<n;i++){
+            V+=ciphers[i]/(k[i]*S);
+        }
+        V=V-ciphers[n]/(k[n]*s[n]*S);
+        return V;
+    }
+
+    /*instance 3*/
+    double[] enc3(double[] k, double[] s, double V, double[] r, double[] p){
+        int n=r.length-1;
+        double [] ciphers =new double[k.length];
+        ciphers[0]=k[0]*(s[0]*V+p[0]/k[1]-r[n]/k[n])+r[0]-p[n];
+        for(int i=1;i<n;i++){
+            ciphers[i]=k[i]*(s[i]*V+p[i]/k[i+1]-r[i-1]/k[i-1])+r[i]-p[i-1];
+        }
+        ciphers[n]=k[n]*(s[n]*V+p[n]/k[0]-r[n-1]/k[n-1])+r[n]-p[n-1];
+        return ciphers;
+    }
+    double dec3(double[] k, double[] s, double[] ciphers){
+        double V=0;
+        int n=k.length-1;
+        double S=sum(s);
+        for(int i=0;i<=n;i++){
+            V+=ciphers[i]/(k[i]*S);
+        }
+        return V;
+    }
+
+
+
+
+    double sum(double[] p, double[] r, double[] k,int n){
+        double sum=0;
+        for(int i=0;i<n;i++){
+            sum+=p[i]+r[i]/k[i];
+        }
+        return sum;
+    }
+
+    double sum(double[] r,int n){
+        double sum=0;
+        for(int i=0;i<n;i++){
+            sum+=r[i];
+        }
+        return sum;
+    }
+
+
+    double sum(double[] r) {
+        double sum=0;
+        for(double temp:r){
+            sum+=temp;
+        }
+        return sum;
+    }
+
+
 }
--- src/test/java/arslab/amber/PatentTest.java
+++ src/test/java/arslab/amber/PatentTest.java
@@ -2,7 +2,12 @@ package arslab.amber;
 
 import static org.junit.Assert.assertTrue;
 
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Unit test for simple App.
@@ -12,9 +17,87 @@ public class PatentTest
     /**
      * Rigorous Test :-)
      */
+    Patent patent=new Patent();
+    double[]k={-735.3,523.2,5261.8};
+    double[]s={0.023,-0.015,0.0078};
+    double[]V={1383.2,2965.8,1383.2,2965.7,3196.1};
+    double[]r1={178.2,4789.3,378.8};
+    double[]r2={7455.1,409.2,7809.2};
+    double[]r3={2946.8,9163.5,-4236.3};
+    double[]r4={132.3,-5734.4,89.34};
+    double[]r5={1945.5,905.3,6734.1};
+
+    double[]p1={569.5,-7836.1,1449.2};
+    double[]p2={4658.2,5068.3,240.4};
+    double[]p3={793.8,5723.9,-4372.3};
+    double[]p4={6489.2,1644.5,2976.6};
+    double[]p5={-593.7,2789.1,989.2};
+    List<double[]>r=new ArrayList<>();
+    List<double[]>p=new ArrayList<>();
+
+    @Before
+    public void setup(){
+        r.add(r1);
+        r.add(r2);
+        r.add(r3);
+        r.add(r4);
+        r.add(r5);
+
+
+        p.add(p1);
+        p.add(p2);
+        p.add(p3);
+        p.add(p4);
+        p.add(p5);
+    }
+
     @Test
     public void shouldAnswerWithTrue()
     {
         assertTrue( true );
     }
+
+    @Test
+    public void instance1(){
+        double result;
+        int i=0;
+        for(double v:V){
+            double []cipher=patent.enc1(k,v,r.get(i));
+            result=patent.dec1(k,cipher,r.get(i));
+            i++;
+            System.out.printf("Expected:%f Actual:%f\n",v,result);
+            Assert.assertEquals(v, result, 0.00000001);
+        }
+    }
+
+    @Test
+    public void instance2(){
+        double result;
+        int i=0;
+        for(double v:V){
+            double []cipher=patent.enc2(k, s, v, r.get(i), p.get(i));
+            result=patent.dec2(k, s, cipher, r.get(i), p.get(i));
+            i++;
+            System.out.printf("Expected:%f Actual:%f\n",v,result);
+            Assert.assertEquals(v, result, 0.00000001);
+        }
+    }
+    @Test
+    public void instance3(){
+        double result;
+        int i=0;
+        for(double v:V){
+            double []cipher=patent.enc3(k,s,v,r.get(i),p.get(i));
+            result=patent.dec3(k,s,cipher);
+            i++;
+            System.out.printf("Expected:%f Actual:%f\n",v,result);
+            Assert.assertEquals(v, result, 0.00000001);
+        }
+    }
+
+
+
+
+
+
 }
