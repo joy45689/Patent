33604baed2a4255a4b14d32012af1d426d0b434e
joy45689 <joy45689acc@gmail.com>
2019-02-22 13:36:45 +0800

    Implementation of the patent

---
+++ src/main/java/arslab/amber/Patent.java
@@ -0,0 +1,13 @@
+package arslab.amber;
+
+/**
+ * Hello world!
+ *
+ */
+public class Patent
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/arslab/amber/PatentTest.java
@@ -0,0 +1,20 @@
+package arslab.amber;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class PatentTest
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
